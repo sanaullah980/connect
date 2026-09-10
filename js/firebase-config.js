7d6ee9e658602a61fdf6f27502e03229b8e16b08
Sanaullah <sanaullahmawia980@gmail.com>
2026-09-10 10:55:42 -0700

    rtfyguihj

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -1,6 +1,13 @@
-/* Paste your Firebase Web App config here before deployment. */
+/* Firebase Web App configuration for Connect Campus. */
 window.CC_FIREBASE_CONFIG = {
-  apiKey: "", authDomain: "", projectId: "", storageBucket: "", messagingSenderId: "", appId: ""
+  apiKey: "AIzaSyAeNS-BF2NQnRY-FoQ1FUzA8cGL7nRVgGM",
+  authDomain: "pafiast-campus.firebaseapp.com",
+  projectId: "pafiast-campus",
+  storageBucket: "pafiast-campus.firebasestorage.app",
+  messagingSenderId: "225899557462",
+  appId: "1:225899557462:web:ae857dfbbf92a2844741f7",
+  measurementId: "G-QFMLSV84HZ"
 };
-/* The only account that can open Owner Admin / Assign Roles / Database. */
+
+/* The only Google account that can open Owner Admin / Assign Roles / Database. */
 window.CC_OWNER_GOOGLE_EMAIL = "sanaullahmawia980@gmail.com";
